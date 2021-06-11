97d400e10362ebeda6f4e6e4988a69ffb34656cf
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-06-11 10:06:46 +0530

    Update subarraysum1.cpp

--- subarraysum1.cpp
+++ subarraysum1.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main()
 {
-	int n,s;
-	cin>>n;
+    int n,s;
+    cin>>n;
     int arr[n];
     cout<<"enter sum\n";
     cin>>s;
     for(int i=0;i<n;i++)
     {
     	cin>>arr[i];
-	}
+    }
     for (int i=0; i <n; i++)
     {
         for (int j=i; j<=n; j++)
@@ -19,11 +19,11 @@ int main()
             for (int k=i; k<=j; k++)
             {
                 sum+=arr[k];
- 			}
- 			if(sum==s)
- 			{
- 				cout<<"YES";
-			}
+ 	    }
+ 	    if(sum==s)
+ 	    {
+ 		cout<<"YES";
+	    }
         }
     }
     return 0;
