17fd5123cc4fe02565b8dcb4f4132078f27830b2
SaiSree-0517 <69571289+SaiSree-0517@users.noreply.github.com>
2021-06-11 09:43:12 +0530

    Add files via upload

---
+++ subarraysum.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,s,c=0;
+	cin>>n;
+    int arr[n];
+    cout<<"enter sum\n";
+    cin>>s;
+    for(int i=0;i<n;i++)
+    {
+    	cin>>arr[i];
+	}
+    for (int i=0; i <n; i++)
+    {
+        for (int j=i; j<n; j++)
+        {
+            for (int k=i; k<=j; k++)
+            {
+                if(arr[k]+arr[k+1]==s)
+                {
+                	c++;
+				}
+ 			}
+        }
+    }
+    if(c!=0)
+    {
+    	cout<<"YES"<<endl;
+	}
+	else
+	{
+		cout<<"NO\n";
+	}
+    return 0;
+}
